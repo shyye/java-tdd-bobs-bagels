356f423878c33bd531524d59ae683f25589f5041
shyye <emma.art.dev@gmail.com>
2024-08-14 15:09:49 +0200

    Update code

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -2,6 +2,7 @@ package com.booleanuk.core;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Basket {
@@ -21,6 +22,26 @@ public class Basket {
             System.out.println("Basket is full, can't add more items.");
             return false;
         }
+
+        // List properties
+        ArrayList<Integer> details = new ArrayList<>();
+        int numOfProducts;
+        int price = 0;  // TODO: Placehlder value
+
+        // Update numOfProducts if already exist, otherwise just add
+        if (products.containsKey(product)) {
+            int previousNumOfProducts = products.get(product).get(0);   // Index 0 is numOfProduct, Index 1 is price
+            numOfProducts = previousNumOfProducts++;
+        } else {
+            numOfProducts = 1;
+        }
+
+        // Add product details
+        details.add(numOfProducts);
+        details.add(price);
+
+        // Add product
+        products.put(product, details);
         return true;
     }
 }
