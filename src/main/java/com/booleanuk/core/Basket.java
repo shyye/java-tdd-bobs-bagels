17bbfad84b7a33c2fa139eeec2d7457ad2704fa3
shyye <emma.art.dev@gmail.com>
2024-08-14 14:52:28 +0200

    Update code

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,9 +1,26 @@
 package com.booleanuk.core;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Basket {
+    // TODO: Difference between ArrayList and simple List??
+    // TODO: Should this be private?
+    HashMap<String, ArrayList<Integer>> products;
+    int maxCapacity;
+
+    public Basket() {
+        this.products = new HashMap<>();
+        this.maxCapacity = 3;
+    }
 
     public boolean add(String product) {
-        return false;
+        // Check if max capacity
+        if (products.size() >= maxCapacity) {
+            System.out.println("Basket is full, can't add more items.");
+            return false;
+        }
+        return true;
     }
 }
