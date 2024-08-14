6211ef9d33e23db923e797c98daf7fd3b19015c8
shyye <emma.art.dev@gmail.com>
2024-08-14 15:32:42 +0200

    Add/update test for max capacity

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -13,10 +13,13 @@ class BasketTest {
     public void add() {
         Basket basket = new Basket();
         Assertions.assertTrue(basket.add("Unicorn Bagel"));
-        Assertions.assertEquals(1, basket.products.size());
+        Assertions.assertEquals(1, basket.size);
 
         basket.add("Unicorn Bagel");
         Assertions.assertEquals(2, basket.products.get("Unicorn Bagel").get(0));    // Index 0 is numOfProducts
+
+        basket.add("Sunhaven Bagel");
+        Assertions.assertFalse(basket.add("Moon Bagel"));    // Exceed maxCapacity
     }
 
 //    @Test
