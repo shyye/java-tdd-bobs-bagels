b265ccd2c096cd314abf461623372045ddf191a6
shyye <emma.art.dev@gmail.com>
2024-08-14 14:24:40 +0200

    Remove code / move code to test

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,12 +1,7 @@
 package com.booleanuk.core;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Basket {
-    // TODO: Difference between ArrayList and simple List??
-    HashMap<String, ArrayList<Integer>> products = new HashMap<>();
-    int maxCapacity = 3;
 
     public boolean add(String product) {
         return false;
