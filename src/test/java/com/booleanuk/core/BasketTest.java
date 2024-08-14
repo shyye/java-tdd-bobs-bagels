9cd54ad2371d66159a306395425aae312081729f
shyye <emma.art.dev@gmail.com>
2024-08-14 14:02:53 +0200

    Add add() test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -3,6 +3,14 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class BasketTest {
 
+    // User story 1.
+    @Test
+    public void add() {
+        Basket basket = new Basket();
+        basket.add("Unicorn Bagel");
+    }
 }
